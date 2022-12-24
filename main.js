5a08d4da450622e28e402566bc3ffa3f4eda2377
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-24 15:12:12 -0500

    added eyes, changed head to be a group

--- main.js
+++ main.js
@@ -67,10 +67,18 @@ class Robot {
 
   // Methods for creating each part of the robot
   createHead() {
+    // Create a new group for the head
+    this.head = new THREE.Group();
+
     const geo = new THREE.BoxGeometry(1.4, 1.4, 1.4);
-    const head = new THREE.Mesh(geo, blueRobotMaterial);
-    head.position.y = 1.65; // Position it above the body
-    this.group.add(head); // Add it to our group
+    const skull = new THREE.Mesh(geo, blueRobotMaterial);
+
+    this.head.add(skull); // Add the skull to our head group
+    this.group.add(this.head); // Add the 'head group' to our 'this.group'
+
+    this.head.position.y = 1.65; // Position 'head group' above the body
+
+    this.createEyes(); // Create eyes
   }
   createBody() {
     const geo = new THREE.BoxGeometry(1, 1.5, 1); // A 'tall' box
@@ -79,28 +87,61 @@ class Robot {
     this.group.add(body); // Add it to this Robot's group.
   }
   createArms() {
+    const height = 1; // Used for positioning the 'transform origin'
     const geo = new THREE.BoxGeometry(0.25, 1, 0.25); // A skinny box
     // Make two of them -
     for (let i = 0; i < 2; ++i) {
+      // Because the pivot point cannot be changed on this geo, we need to make a new group -
+      // - add this armGroup to our 'this' and Then we can rotate the group.
+      const armGroup = new THREE.Group(); // Create a group for an arm
       const arm = new THREE.Mesh(geo, blueRobotMaterial);
-      this.group.add(arm);
 
-      /* Position each arm on either side of the body. 
+      armGroup.add(arm); // add the arm to the armGroup
+      this.group.add(armGroup); // ad the armGroup to the 'this.group'
+
+      /* Position each armGroup on either side of the body. 
       I find it helpful here to create a variable 'multiplier'.
       This helps us position the left arm in the opposite direction on the x axis,
       Also used for rotating the arms. */
       const indexIsEven = i % 2 === 0;
       const multiplier = indexIsEven ? 1 : -1;
-      arm.position.x = multiplier * 0.8;
-      arm.position.y = 0.1;
-      arm.rotation.z = degreesToRadians(30 * multiplier); // Have arms 'stick out'
+
+      arm.position.y = height * -0.5; // Translate the arm (not armGroup) down by half the height
+      armGroup.position.x = multiplier * 0.8; // Other positioning is applied to the group
+      armGroup.position.y = 0.6;
+
+      // Visual Helper - goes at the END but before we rotate (or else positions are outdated)
+      const box = new THREE.BoxHelper(armGroup, 0xffff00);
+      this.group.add(box);
+
+      armGroup.rotation.z = degreesToRadians(30 * multiplier); // Have arms 'stick out'
     }
+  }
+  createEyes() {
+    const geo = new THREE.SphereGeometry(0.15, 12, 8);
+    const eyeMaterial = new THREE.MeshLambertMaterial({ color: 0xff0000 });
+
+    // Create eye group (makes moving both at the same time easier)
+    const eyes = new THREE.Group();
+    for (let i = 0; i < 2; ++i) {
+      const eye = new THREE.Mesh(geo, eyeMaterial);
+      const m = i % 2 === 0 ? 1 : -1; // 'multiplier' for doing things to left or right accordingly
+      eyes.add(eye);
+      eye.position.x = 0.36 * m;
+    }
+
+    this.head.add(eyes); // add the eyes group to our head group
+
+    // Move the eyes forwards by half of the head depth -
+    // it might be a good idea to create a variable to do this!
+    eyes.position.z = 0.7;
   }
   // Method that Initializes the figure (call all the 'creates' methods)
   init() {
     this.createHead();
     this.createBody();
     this.createArms();
+    // createEyes is called inside createHead
   }
 }
 
